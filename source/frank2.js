92003459e98dfd5f26cbd70ea638eb40a27019b6
MaximPerry <maxim_perry@live.ca>
2019-03-28 17:09:22 -0400

    Initial commit

---
+++ source/frank2.js
@@ -0,0 +1,48 @@
+Bots.push({
+    "name": "frank2",
+    "precatch": function(text) {
+        return text;
+    },
+    "actions": [
+        {
+            "catch": ["^hello^"],
+            "response": "Hello!"
+        },
+        {
+            "catch": ["*hellow*"],
+            "response": [ 
+                {
+                    "text": "I think it is written like 'hello'?",
+                    "actions": [
+                        {
+                            "catch": ["*yes*"],
+                            "response": "No biggie! Howdy to you ;-)"
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "catch": ["^how are you^"],
+            "response": "I'm not exactly the kind to have feelings. But thanks for asking :-D! How about you?"
+        }, {
+            "catch": ["^I'm good^"],
+            "response": "My name is Frank II. What's yours?"
+        }, 
+        {
+            "catch": ["Can you do the dishes?"],
+            "response": "You wish."
+        }
+    ],
+    "postcatch": function(text) {
+        return text;
+    },
+    
+    "functions": {},
+    "synonyms": [
+        ["hello", "hi", "morning", "good morning", "good afternoon", "good evening", "*hey*"],
+        ["how are you", "how do you do"],
+        ["I'm good", "good", "I'm okay", "okay", "I'm alright", "alright", "*fine*"]
+    ],
+    "variables": {}
+});
